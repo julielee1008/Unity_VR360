8918bb8110549250355fe0d659dd138cf28cf9fd
julielee1008 <julielee1008@gmail.com>
2020-12-15 22:53:19 +0800

    summry

---
+++ VR360/Assets/scripts/gm.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class gm : MonoBehaviour
+{
+    public int score;
+    public Text uiScore;
+
+
+    public void AddScore()
+    {
+        score += 10;
+        uiScore.text = "分數:" + score;
+    }
+
+
+
+}
+
